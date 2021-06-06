05b0a3897fdcc34569dfd243719332bae6a95253
Ayushak18 <aayushkamboj7@gmail.com>
2021-06-06 18:50:32 +0530

    Owner

---
+++ block-BNaadz/Blog2/middlewares/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  checkLogin: (req, res, next) => {
+    if (req.session && req.session.userId) {
+      next();
+    } else {
+      res.redirect('/users/login');
+    }
+  },
+};
